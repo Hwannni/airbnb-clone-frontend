030b40cb235a8440f3a2bd959413519fe2d1758b
Hwannni <cmhcms1115@naver.com>
2024-08-29 01:41:28 +0900

    18_0_Header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
+
+{/* Outlet은 루트의 중앙에 우리가 렌더링 하고싶은 컴포넌트를 위치시킨다. */}
 export default function Root() {
   return (
-    <h1>
-      im root
-      {/* Outlet은 루트의 중앙에 우리가 렌더링 하고싶은 컴포넌트를 위치시킨다. */}
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={5}
+        px={10}
+        borderBottomWidth={1}
+      >
+        <Box color="red.500">
+          <Link to={"/"}>
+            <FaAirbnb size={"48"} />
+          </Link>
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
+
