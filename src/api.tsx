7e36d5146ce9816acae9fa30b15d52652890a8ac
Hwannni <cmhcms1115@naver.com>
2024-08-30 14:33:37 +0900

    19_3_Axios

--- src/api.tsx
+++ src/api.tsx
@@ -1,10 +1,10 @@
 // API를 fetch하기 위해 적었던 모든 함수들을 가져온다.
 // fetch코드를 컴포넌트에 적지 않기 위함
+import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+});
 
-export async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
-}
+export const getRooms = () =>
+  instance.get("rooms/").then((response) => response.data);
